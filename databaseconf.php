32b0788f20dcb0afa86b35d9e5c519d027f30acb
weihuagu <ministarmail@msn.cn>
2018-02-03 10:14:33 +0800

    oo

--- databaseconf.php
+++ databaseconf.php
@@ -2,9 +2,10 @@
 return 
 [
         'database_type' => 'mysql',
-        'database_name' => 'tang_poetry',
-        'server' => 'localhost',
-        'username' => 'root',
-        'password' => '1992',
+        'database_name' => $_ENV['MYSQL_DBNAME'],
+        'server' => $_ENV['MYSQL_HOST'],
+        'username' => $_ENV['MYSQL_USERNAME'],
+        'password' =>$_ENV['MYSQL_PASSWORD'],
+        'port' => $_ENV['MYSQL_PORT']
         'charset' => 'utf8'
     ];
